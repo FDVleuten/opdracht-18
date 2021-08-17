5372cf77a3a3826af43c846d74ccd9f8e119dda0
FDVleuten <86839267+FDVleuten@users.noreply.github.com>
2021-08-17 11:32:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+/* Function declaration
+function add(number1, number2) {
+  const sum1 = ((number1 * number1) + (number2 * number2)) * ((number1 * number1) + (number2 * number2));
+  return sum1
+};
+const result = add(4, 5);
+console.log(result);
+*/
+
+/* function expression
+const add = function (number1, number2) {
+  const sum1 = ((number1 * number1) + (number2 * number2)) * ((number1 * number1) + (number2 * number2));
+  return sum1
+};
+const result = add(4, 5);
+console.log(result);
+*/
+
+// arrouw function
+const add = () => {
+const sum1 ((number1 * number1) + (number2 * number2)) * ((number1 *number1) + (number2 * number2));
+  return sum1;
+  };
+  const result = add(4, 5);
+    console.log(result);
+};
+
+
